44953dc82365cc40811b1d91ee1a6d448e419e0b
cleverCoader <adventure.developer45@gmail.com>
2020-09-23 11:42:23 +0530

    feed component sturuture

---
+++ src/Feed.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Feed.css';
+
+function Feed() {
+    return (
+        <div className="feed" >
+            {/* story component*/}
+            {/* message sender  component*/}
+
+        </div>
+    )
+}
+
+export default Feed;
