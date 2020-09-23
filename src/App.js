728079aa3ea35bbf83e4fd76d314886041d9b28d
cleverCoader <adventure.developer45@gmail.com>
2020-09-24 00:05:00 +0530

    all the way to login page now

--- src/App.js
+++ src/App.js
@@ -4,17 +4,30 @@ import Header from './Header';
 import Sidebar from './Sidebar';
 import Feed from './Feed';
 import Widgets from './Widgets';
+import Login from './Login';
+
 
 function App() {
+
+  const user = null;
+
+
   return (
     <div className="app">
-      <Header />
-
-      <div className="app_body">
-        <Sidebar />  
-        <Feed />
-        <Widgets />
-      </div>
+      {!user ? (
+        // <h1> please login !</h1>
+        <Login />
+      ) : (
+          <>
+          <Header />
+          <div className="app_body">
+            <Sidebar />  
+            <Feed />
+            <Widgets />
+          </div>
+          
+          </>
+      )}
 
     </div>
   );
