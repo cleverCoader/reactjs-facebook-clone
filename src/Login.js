7b1b193926c33b40ff39a4fb2605493c23c89308
cleverCoader <adventure.developer45@gmail.com>
2020-09-24 00:33:37 +0530

    login page created but firebase is unable to install in project

--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import { Button } from '@material-ui/core';
+import React from 'react';
+import './Login.css';
 
 function Login() {
+
+    const signIn = () => {
+        
+    }
+
     return (
         <div className="login" >
-            Logging In
+            <div className="login_logo">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/600px-Facebook_f_logo_%282019%29.svg.png" alt=""/>
+            <img src="https://www.logo.wine/a/logo/Facebook/Facebook-Logo.wine.svg" alt="" />
+            <button type="submit" onClick={signIn}>Sign In</button>
+            </div>
         </div>
     )
 }
