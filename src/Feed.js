58bd0f89ebe700e5315cbb0fc32a2e36751ae7b7
cleverCoader <adventure.developer45@gmail.com>
2020-09-23 22:20:16 +0530

    amazing posts part is done

--- src/Feed.js
+++ src/Feed.js
@@ -11,9 +11,48 @@ function Feed() {
         <div className="feed" >
             <Stories />
             <MessageSender />
-            <Post />
-            <Post />
-            <Post />
+
+
+            <Post
+                profilePic="https://avatars0.githubusercontent.com/u/71284942?s=460&u=042477abf4c5020157b30e8506b311a02d37f492&v=4"
+                message="Hii this is my build"
+                timestamp="23rd sept 2020"
+                username="samsan"
+                image="https://wallpapercave.com/wp/wp7494287.jpg"
+            />
+
+            <Post
+                profilePic="https://avatars0.githubusercontent.com/u/71284942?s=460&u=042477abf4c5020157b30e8506b311a02d37f492&v=4"
+                message="Hii this is my build"
+                timestamp="23rd sept 2020"
+                username="samsan"
+                image="https://wallpapercave.com/wp/wp1859057.jpg"
+            />
+
+            <Post
+                profilePic="https://avatars0.githubusercontent.com/u/71284942?s=460&u=042477abf4c5020157b30e8506b311a02d37f492&v=4"
+                message="Hii this is my build"
+                timestamp="23rd sept 2020"
+                username="samsan"
+                image="https://wallpapercave.com/wp/wp5193539.png"
+            />
+
+            <Post
+                profilePic="https://avatars0.githubusercontent.com/u/71284942?s=460&u=042477abf4c5020157b30e8506b311a02d37f492&v=4"
+                message="Hii this is my build"
+                timestamp="23rd sept 2020"
+                username="samsan"
+                image="https://wallpapercave.com/wp/wp1894272.jpg"
+            />
+
+            <Post
+                profilePic="https://avatars0.githubusercontent.com/u/71284942?s=460&u=042477abf4c5020157b30e8506b311a02d37f492&v=4"
+                message="Hii this is my build"
+                timestamp="23rd sept 2020"
+                username="samsan"
+                image="https://wallpapercave.com/wp/wp1897575.jpg"
+            />
+            
         </div>
     )
 }
