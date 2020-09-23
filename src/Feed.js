68a65e79c59fc22cb139b1ff9e677fd945cd7605
cleverCoader <adventure.developer45@gmail.com>
2020-09-23 13:09:28 +0530

    building stories and story part

--- src/Feed.js
+++ src/Feed.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Feed.css';
 
+import Stories from './Stories';
+
 function Feed() {
     return (
         <div className="feed" >
-            {/* story component*/}
+            <Stories />
             {/* message sender  component*/}
 
         </div>
---
+++ src/Stories.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './Stories.css';
+
+import Story from './Story';
+
+function Stories() {
+    return (
+        <div className="stories">
+            <Story
+                image="https://wallpapercave.com/wp/wp1894262.jpg"
+                profileSrc="https://wallpapercave.com/wp/wp1894262.jpg"
+                title="A B Devilliars"
+            />
+            <Story
+                image="https://wallpapercave.com/wp/wp1897575.jpg"
+                profileSrc="https://wallpapercave.com/wp/wp1897575.jpg"
+                title="A B Devilliars"
+            />
+            <Story
+                image="https://wallpapercave.com/wp/wp1894262.jpg"
+                profileSrc="https://wallpapercave.com/wp/wp1894262.jpg"
+                title="A B Devilliars"
+            />
+            <Story
+                image="https://wallpapercave.com/wp/wp1894262.jpg"
+                profileSrc="https://wallpapercave.com/wp/wp1894262.jpg"
+                title="A B Devilliars"
+            />
+            <Story
+                image="https://wallpapercave.com/wp/wp1894262.jpg"
+                profileSrc="https://wallpapercave.com/wp/wp1894262.jpg"
+                title="A B Devilliars"
+            />
+            
+        </div>
+    )
+}
+
+export default Stories;
